96e3609b7692abd5d7304ca250500a4ce0da9c95
quocdk <quocdkph28915@fpt.edu.vn>
2024-04-06 21:58:22 +0700

    Sửa models và config 2

---
+++ AppData/Configurations/PinConfig.cs
@@ -0,0 +1,22 @@
+﻿using App_Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Configurations
+{
+    public class PinConfig : IEntityTypeConfiguration<Pin>
+    {
+        public void Configure(EntityTypeBuilder<Pin> builder)
+        {
+            builder.ToTable("Pin");
+            builder.HasKey(c => c.IdPin);
+            builder.Property(c => c.LoaiPin).IsRequired().IsUnicode().HasMaxLength(50);
+            
+        }
+    }
+}
---
+++ AppData/Configurations/TheNhoConfig.cs
@@ -0,0 +1,22 @@
+﻿using App_Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Configurations
+{
+    public class TheNhoConfig : IEntityTypeConfiguration<TheNho>
+    {
+        public void Configure(EntityTypeBuilder<TheNho> builder)
+        {
+            builder.ToTable("TheNho");
+            builder.HasKey(c => c.IdTheNho);
+            builder.Property(c => c.LoaiTheNho).IsRequired().IsUnicode().HasMaxLength(50);
+            
+        }
+    }
+}
